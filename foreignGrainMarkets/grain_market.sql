5122829eaf8472c641eff3ba03e90d97d288ce08
Ashish Mistry <ashishmistry19@yahoo.com>
2022-06-02 19:22:37 -0400

    Completed exercises

--- foreignGrainMarkets/grain_market.sql
+++ foreignGrainMarkets/grain_market.sql
@@ -32,22 +32,37 @@ limit 1;
 
 -- Problem 9: Which crops have ever reached a farm price greater than 
 -- 			  or equal to $350 per metric tonne?
-
+select distinct CROP_TYPE from monthly_farm_prices
+where PRICE_PRERMT>=350;
 
 -- Problem 10: Rank the crop types harvested in Saskatchewan in the year
 -- 			   2000 by their average yield. Which crop performed best?
-
+select crop_type,geo,avg_yield from annual_crop_data
+where geo = "Saskatchewan" and year(year)=2000
+order by AVG_YIELD desc;
 
 -- Problem 11: Rank the crops and geographies by their average yield 
 -- 	   		   (KG per hectare) since the year 2000. Which crop 
 -- 			   and province had the highest average yield since the year 2000?
+select year,crop_type,geo,avg_yield from annual_crop_data
+where year(year)>=2000
+order by AVG_YIELD desc;
 
-
--- Problem 12:  to determine how much wheat was harvested in Canada
+-- Problem 12: determine how much wheat was harvested in Canada
 -- 			   in the most recent year of the data.
+select crop_type,HARVESTED_AREA,YEAR(year) as "Year",geo from annual_crop_data
+where year(year) = (select max(year(year)) from annual_crop_data) 
+	and geo="Canada" and crop_type="wheat";
 
-
--- Problem 13: calculate the monthly price per metric tonne
---  of Canola grown in Saskatchewan in both Canadian and US dollars. Display the most recent 6 months of the data.
-
+-- Problem 13: calculate the monthly price per metric tonne of Canola grown in Saskatchewan in both Canadian and US dollars. 
+-- Display the most recent 6 months of the data. order by the month desc limit 6
+select fx.DATE,farm.geo,farm.CROP_TYPE,
+		cast((fx.FXUSDCAD*farm.PRICE_PRERMT) as decimal(10,2)) as price_CAN,
+		farm.PRICE_PRERMT as price_USD 
+		from monthly_fx as fx,monthly_farm_prices as farm
+where fx.DATE = farm.DATE 
+	and farm.geo = "Saskatchewan" 
+	and farm.crop_type="Canola"
+order by fx.date desc
+limit 6;
 
